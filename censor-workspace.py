1ffa1d0fbbe66b5ba463f77c94b59de5035ca2c9
MedicodiBiscotti <13235260+MedicodiBiscotti@users.noreply.github.com>
2024-12-05 10:41:14 +0100

    refactor to more methods

--- censor-workspace.py
+++ censor-workspace.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 # str.encode should output needed bytes (byte string, I think).
 # json.loads can read bytes in UTF which workspace should be in, I think.
@@ -7,18 +8,19 @@ import json
 
 
 def read_and_write_back(input: str, output: str):
-    with open(input) as infile:
-        data = json.load(infile)
-        write_json_string(data, output)
+    data = read_data(input)
+    write_json_string(data, output)
 
 
 def read_and_write_back_bytes(input: str, output: str):
+    data = read_data(input)
+    write_json_bytes(data, output)
+
+
+def read_data(input: str) -> dict:
     with open(input) as infile:
         data = json.load(infile)
-        data_str = format_json_string(data)
-        data_bytes = data_str.encode()
-        with open(output, "wb") as outfile:
-            outfile.write(data_bytes)
+        return data
 
 
 def write_json_file(data: dict, output: str):
@@ -32,9 +34,33 @@ def format_json_string(data: dict) -> str:
 
 
 def write_json_string(data: dict, output: str):
+    data_str = format_json_string(data)
     with open(output, "w", newline="\n") as outfile:
-        data_str = format_json_string(data)
         outfile.write(data_str)
+
+
+def write_json_bytes(data: dict, output: str):
+    data_str = format_json_string(data)
+    data_bytes = data_str.encode()
+    with open(output, "wb") as outfile:
+        outfile.write(data_bytes)
+
+
+def main(input: str, output: str):
+    data = read_data(input)
+    data = censor_sensitive_information(data)
+    write_json_bytes(data, output)
+
+
+def censor_sensitive_information(data: dict) -> dict:
+    banned_file_patterns = [
+        re.compile(pattern)
+        for pattern in [
+            r"Some company/.*\.md",
+            r"Another company/.*\.md",
+        ]
+    ]
+    banned_search_words = ["secret"]
 
 
 if __name__ == "__main__":
